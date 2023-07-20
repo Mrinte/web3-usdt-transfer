38bf963b5254ebc1b0a7a8d58549f3e09f1d74f3
Mrinte <617893305@qq.com>
2023-07-21 06:04:19 +0800

    v1.0.0
    本插件用于币安智能链（BNB Chain）的USDT插件转账，其它代币可自行修改ABI和网络URL、合约ABI进行使用

---
+++ Web3-USDT-Transfer/README.md
@@ -0,0 +1,2 @@
+# Web3-USDT-Transfer
+本插件用于币安智能链（BNB Chain）的USDT插件转账，其它代币可自行修改ABI和网络URL、合约ABI进行使用；
---
+++ Web3-USDT-Transfer/src/Web3USDTTransfer.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 本插件用于币安智能链（BNB Chain）的USDT插件转账，其它代币可自行修改ABI和网络URL、合约ABI进行使用；
+ * (c) MrInte <617893305@qq.com>
+ * @author MrInte <617893305@qq.com>
+ * @license MIT
+ */
+
+namespace MrInte\Web3USDTTransfer;
+
+use Web3\Web3;
+use Web3\Providers\HttpProvider;
+use Web3\RequestManagers\HttpRequestManager;
+use Web3\Contract;
+use Web3\Utils;
+use Web3p\EthereumTx\Transaction;
+use EthTool\Callback;
+
+class Web3USDTTransfer{
+    public function USDTTransfer(){
+        $Web3 = new Web3();
+        return "dddddddddd";
+    }
+}
